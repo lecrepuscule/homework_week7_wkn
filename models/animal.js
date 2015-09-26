c083ff4d44a41617faad0605e84dcc4865cc2d81
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-26 12:40:59 +0100

    added Animal model

---
+++ models/animal.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+
+var animalSchema = new mongoose.Schema({
+  name: String,
+  breed: String,
+  dob: Date,
+  gender: String,
+  family: String,
+  status: String,
+  updatedAt: {type: Date, default: Date.now}
+});
+
+var Animal = mongoose.model("Animal", animalSchema);
+
+module.exports = Animal;
